6456f687115eb3928b22cd073053e97ed33b7563
Seghier Dahmane <dream787@outlook.com>
2018-12-23 20:48:30 +0100

    manual initial commit

---
+++ WalkOnRetailAdmin/www/scripts/utils.js
@@ -0,0 +1,16 @@
+﻿
+function crt_elt(tagname, parent) {
+    var elt = document.createElement(tagname);
+    if (parent) {
+        parent.appendChild(elt);
+    }
+    return elt;
+}
+
+function style(elt, styles) {
+    for (var p in styles) {
+        if (styles.hasOwnProperty(p)) {
+            elt.style[p] = styles[p];
+        }
+    }
+}
